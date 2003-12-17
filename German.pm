9969e66352ea4a905a72f946f47b12bb0e97df80
Christian Winter <chrwin@cpan.org>
2003-12-17 22:14:03 +0000

    Import of CHRWIN/date-spoken-german-0.03 from CPAN.
    gitpan-cpan-distribution: Date-Spoken-German
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         CHRWIN/date-spoken-german/date-spoken-german-0.03.tar.gz
    gitpan-cpan-author:       CHRWIN
    gitpan-cpan-maturity:     released

--- German.pm
+++ German.pm
@@ -10,75 +10,85 @@ Date::Spoken::German - Output dates as ASCII text as you would speak it
 
 =head1 SYNOPSIS
 
-  use Date::Spoken::German;
+ use Date::Spoken::German;
 
-  print timetospoken( time() );
-  print datetospoken( $DAY, $MONTH, $YEAR );
+ print timetospoken( time() );
+ print datetospoken( $DAY, $MONTH, $YEAR );
 
 =head1 DESCRIPTION
 
-  This module provides you with functions to easily convert a date (given
-  as either integer values for day, month and year or as a unix timestamp)
-  to its representation as german text, like you would read it aloud.
+This module provides you with functions to easily convert a date (given
+as either integer values for day, month and year or as a unix timestamp)
+to its representation as german text, like you would read it aloud.
 
 =head1 EXPORTABLE TAGS
 
-  :ALL    - all helper methods are also exported into the callers namespace
+:ALL    - all helper methods are also exported into the callers namespace
 
 =head1 FUNCTIONS
 
 =head2 Exported by default
 
+=over 2
+
 =item B<timetospoken( $TIMESTAMP )>
 
-  In scalar context, return a string consisting of the text
-  representation of the date in the given unix timestamp,
-  like e.g. "dreizehnter Mai zweitausenddrei".
+In scalar context, return a string consisting of the text
+representation of the date in the given unix timestamp,
+like e.g. "dreizehnter Mai zweitausenddrei".
 
-  In list context, returns the three words of the string as a list.
+In list context, returns the three words of the string as a list.
 
 =item B<datetospoken( $DAY, $MONTH, $YEAR )>
 
-  Takes the values for day of month, month and year as integers
-  (month starting with B<1>) and gives the same return values as
-  I<timetospoken>.
+Takes the values for day of month, month and year as integers
+(month starting with B<1>) and gives the same return values as
+I<timetospoken>.
+
+=back
 
 =head2 Exported by :ALL
 
+=over 2
+
 =item B<yeartospoken( $YEAR )>
 
-  Takes a year (absolute integer value) as input and returns the
-  text representation in German.
+Takes a year (absolute integer value) as input and returns the
+text representation in German.
 
 =item B<monthtospoken( $MONTH )>
 
-  Takes a month (integer value, January = 1) as input and returns
-  the text representation in German.
+Takes a month (integer value, January = 1) as input and returns
+the text representation in German.
 
 =item B<daytospoken( $DAY )>
 
-  Converts a day number to its German text representation.
+Converts a day number to its German text representation.
+
+=back
 
 =head1 KNOWN ISSUES
 
-  None at the moment.
+None at the moment.
 
 =head1 BUGS
 
-  Please report all bugs to the author of this module:
-  Christian Winter <thepoet@a-za-z0-9.de>
+Please report all bugs to the author of this module:
+Christian Winter <thepoet@a-za-z0-9.de>
+
+=for html <a href="mailto:thepoet@a-za-z0-9.de?subject=Bug%20in%20Date::Spoken::German">Mail a Bug</a>
 
 =head1 CREDITS
 
-  To Larry Wall for Perl itself, and to all developers out there
-  contributing to the Perl community. Special thanks to all regulars
-  in the usenet perl groups for giving me a lot of hints that helped
-  me understand what Perl can do.
+To Larry Wall for Perl itself, and to all developers out there
+contributing to the Perl community. Special thanks to all regulars
+in the usenet perl groups for giving me a lot of hints that helped
+me understand what Perl can do.
 
 =head1 SEE ALSO
 
-  B<perldoc perl> on Perl itselft.
-  B<perldoc perlmod> on Perl modules.
+Type B<perldoc perl> to get info on Perl itselft.
+Type B<perldoc perlmod> to get info on Perl modules.
 
 =cut
 
@@ -90,11 +100,12 @@ require POSIX;
 @ISA = qw(Exporter Date::Spoken::German);
 @EXPORT = qw(datetospoken timetospoken);
 %EXPORT_TAGS = ( ALL => [qw(yeartospoken datetospoken timetospoken monthtospoken daytospoken)] );
-$VERSION = "0.02";
+$VERSION = "0.03";
 $AUTHOR = 'Christian Winter <thepoet@a-za-z0-9.de>';
 
 my %cipher = (	1 => "ein", 2 => "zwei", 3 => "drei", 4 => "vier", 5 => "fünf", 6 => "sechs", 7 => "sieben",
-		8 => "acht", 9 => "neun", 10 => "zehn", 11 => "elf", 12 => "zwölf" );
+		8 => "acht", 9 => "neun", 10 => "zehn", 11 => "elf", 12 => "zwölf", 16 => "sechzehn", 17 => "siebzehn", 18 => "achzehn" );
+my %specialcipher = ( 1 => "ers", 3 => "drit", 7 => "sieb", 8 => "ach" );
 my %tens = (	1 => "zehn", 2 => "zwanzig", 3 => "dreissig", 6 => "sechzig", 7 => "siebzig", 8 => "achzig" );
 my %month = (	1 => "Januar", 2 => "Februar", 3 => "März", 4 => "April", 5 => "Mai", 6 => "Juni",
 		7 => "Juli", 8 => "August", 9 => "September", 10 => "Oktober", 11 => "November", 12 => "Dezember" );
@@ -103,15 +114,21 @@ sub yeartospoken
 {
 	my $year = shift;
 	(my $tens = $year) =~ s/^.*(\d\d)$/$1/;
-	my $hundreds;
+	my $hundreds = "";
 	if( $year < 10 ) {
-		$year = $cipher{$year}
+		$year = $cipher{$year} || "null";
 	} else {
-		$tens =~ s/(.)(.)/
-		($1>0)?
-			($cipher{"$1$2"} || $cipher{$2}.(($1!=1)?"und":"").($tens{$1} || $cipher{$1}."zig")):
-			($2>0)?$cipher{$2}.(($2==1)?"s":""):""
-		/ex;
+		if( $tens == 0 ) {
+			$tens = "";
+		} elsif( ($tens % 10) == 0 ) {
+			$tens =~ s/(.)(.)/$tens{$1}/;
+		} else {
+			if( $tens < 10 ) {
+				$tens =~ s/(.)(.)/$cipher{$2}/;
+			} else {
+				$tens =~ s/(.)(.)/$cipher{"$1$2"} || $cipher{$2}.(($1==1)?$tens{$1}:"und".$tens{$1})/e;
+			}
+		}
 		if( $year >= 100 ) {
 			($hundreds = $year) =~ s/^(.?.)..$/$1/;
 			if( $hundreds >= 20 || $hundreds == 10 ) {
@@ -141,7 +158,7 @@ sub daytospoken
 	if( $tag >= 10 ) {
 		$tag =~ s/(.)(.)/$cipher{"$1$2"} || (($2>0)?$cipher{$2}.(($1>1)?"und":""):"").$tens{$1}/ex;
 	} else {
-		$tag = $cipher{$tag};
+		$tag = $specialcipher{$tag} || $cipher{$tag};
 	}
 	return $tag.$endung;
 }
@@ -158,8 +175,7 @@ sub datetospoken
 
 sub timetospoken
 {
-	my $timestamp = shift;
-	my( $tag, $monat, $jahr) = (localtime( shift || time() ))[3,4,5];
+	my( $tag, $monat, $jahr) = (gmtime( shift || time() ))[3,4,5];
 	return datetospoken($tag, $monat+1, 1900+$jahr);
 }
 
